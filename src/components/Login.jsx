9b61dfbe7b3e0ce0672a925104be868d1f1ab691
bhavinparmar005 <bhavinparmar005@gmail.com>
2025-04-16 18:59:39 +0530

    login and sign data aava ,andiyo

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,74 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [input, setInput] = useState({
+    email: "",
+    password: ""
+  })
+
+  const [signData, setSignData] = useState("")
+
+  const handleChange = (e) => {
+    let name = e.target.name
+    let value = e.target.value
+
+    setInput({ ...input, [name]: value })
+
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    // console.log(input);
+    try {
+
+      let filterData = signData.filter((val) => {
+        return (
+          val.email === input.email && val.password === input.password
+        )
+      })
+      if (filterData) {
+        let add = await axios.post("http://localhost:3000/login", input);
+      }
+
+      setInput({
+        email: "",
+        password: ""
+      })
+
+    } catch (error) {
+      console.log(error);
+
+    }
+
+
+  }
+  const getsignData = async () => {
+
+    let r = await axios.get("http://localhost:3000/signup")
+
+    setSignData(r.data)
+  }
+  useEffect(() => {
+    getsignData()
+  }, [])
+
+  console.log(signData);
+
   return (
     <>
-   <div className="container d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <div className="container d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
         <div className="col-md-6 col-lg-4 col-sm-8 ">
           <div className="card p-4 shadow-lg rounded-4">
             <h2 className="text-center mb-4">Login</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
               <div className="mb-3">
                 <label htmlFor="email" className="form-label">Email address</label>
-                <input type="email" className="form-control" id="email" placeholder="Enter your email" required name='email' />
+                <input type="email" className="form-control" id="email" placeholder="Enter your email" required name='email' value={input.email} onChange={handleChange} />
               </div>
               <div className="mb-3">
                 <label htmlFor="password" className="form-label">Password</label>
-                <input type="password" className="form-control" id="password" placeholder="Enter password" required name='password'  />
+                <input type="password" className="form-control" id="password" placeholder="Enter password" required name='password' value={input.password} onChange={handleChange} />
               </div>
               <button type="submit" className="btn btn-primary w-100" >Login</button>
             </form>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,29 +1,60 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Signup = () => {
+    const [input, setInput] = useState({
+        name: "",
+        email: "",
+        confirmPassword: "",
+        password: ""
+    })
+    const handleChange = (e) => {
+        let name = e.target.name
+        let value = e.target.value
+        setInput({ ...input, [name]: value })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        console.log(input);
+        try {
+            let add = await axios.post("http://localhost:3000/signup", input);
+            // setInput({
+            //     name: "",
+            //     email: "",
+            //     confirmPassword: "",
+            //     password: ""
+            // })
+            console.log(add);
+            
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <>
             <div className="container d-flex justify-content-center align-items-center " style={{ height: '100vh' }}>
                 <div className="col-md-6 col-lg-4 col-sm-8">
                     <div className="card p-4 shadow-lg">
                         <h2 className="text-center mb-4">Signup</h2>
-                        <form>
+                        <form onSubmit={handleSubmit}>
                             <div className="mb-3">
                                 <label htmlFor="name" className="form-label">Full Name</label>
-                                <input type="text" className="form-control" id="name" placeholder="Enter your full name" required name='name'  />
+                                <input type="text" className="form-control" id="name" placeholder="Enter your full name" required name='name' value={input.name} onChange={handleChange} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="email" className="form-label">Email address</label>
-                                <input type="email" className="form-control" id="email" placeholder="Enter your email" required name='email'  />
+                                <input type="email" className="form-control" id="email" placeholder="Enter your email" required name='email' value={input.email} onChange={handleChange} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="password" className="form-label">Password</label>
-                                <input type="password" className="form-control" id="password" placeholder="Enter password" required name='password'  />
+                                <input type="password" className="form-control" id="password" placeholder="Enter password" required name='password' value={input.password} onChange={handleChange} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="confirm-password" className="form-label">Confirm Password</label>
-                                <input type="password" className="form-control" id="confirm-password" placeholder="Confirm password" required name='confirmPassword'  />
+                                <input type="password" className="form-control" id="confirm-password" placeholder="Confirm password" required name='confirmPassword' value={input.confirmPassword} onChange={handleChange} />
                             </div>
                             <button type="submit" className="btn btn-primary w-100">Sign-Up</button>
                         </form>
